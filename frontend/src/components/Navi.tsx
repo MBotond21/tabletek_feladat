41a6617aa75d037b0f96d790332742f5dfb364a6
MBotond21 <m.botond07@gmail.com>
2024-11-15 22:20:01 +0100

    kezdolap added to Navi

--- frontend/src/components/Navi.tsx
+++ frontend/src/components/Navi.tsx
@@ -2,6 +2,7 @@ export function Navi() {
     return <>
         <nav>
             <h2>Menü</h2>
+            <a href="/">Kezdőlap</a>
             <a href="/tablet-lista">Tabletek Listája</a>
             <a href="/tablet-felvetel">Tablet Felvétel</a>
             <a href="/tablet-torles">Tablet Törlés</a>
